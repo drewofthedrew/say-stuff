6092dc83774a875015ee17f9b4338864b3e84373
andrew moore <ofthed@ofthed.local>
2022-10-11 20:36:43 -0700

    first push

---
+++ main.py
@@ -0,0 +1,20 @@
+def say_something():
+    print('What do you want to say?')
+    to_print = input()
+    print('How many times do you want to say this?')
+    goal = input()
+
+    count = 0
+    while (count < int(goal)):
+        print(str(count + 1) + f': {to_print}')
+        count += 1
+
+    print(f'Congratulations, you just said {to_print} {goal} times!')
+
+
+def main():
+    say_something()
+
+
+if __name__ == '__main__':
+    main()
